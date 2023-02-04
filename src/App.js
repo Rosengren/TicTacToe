d7dc68376610c01e7a80f3c14ebe459ec8547c38
Kevin Rosengren <kev.rosengren@gmail.com>
2023-02-04 09:00:08 -0800

    Allow players to review a game's history and go to previous moves

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React, { Fragment, useState } from "react";
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(index) {
-    const newSquares = [...squares];
-    if (newSquares[index] || calculateWinner(newSquares)) return;
+    if (squares[index] || calculateWinner(squares)) return;
 
-    if (xIsNext) {
-      newSquares[index] = "X";
-    } else {
-      newSquares[index] = "O";
-    }
-
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
+    const nextSquares = squares.slice();
+    nextSquares[index] = xIsNext ? "X" : "O";
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
